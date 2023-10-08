2c0f9f3400d0fc04ae00c065bc26d9f93247171e
dshzhkv <dashunich.f@gmail.com>
2023-10-08 21:15:38 +0500

    Рскрасила вывод --help с помощью chalk

--- src/core/cli-command/help.command.ts
+++ src/core/cli-command/help.command.ts
@@ -1,4 +1,5 @@
 import { CliCommandInterface } from './cli-command.interface.js';
+import chalk from 'chalk';
 
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
@@ -7,11 +8,11 @@ export default class HelpCommand implements CliCommandInterface {
     console.log(`
         Программа для подготовки данных для REST API сервера.
         Пример:
-            npm run ts ./src/main.cli.ts --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-        `);
+          ${chalk.green('npm run ts ./src/main.cli.ts --<command> [--arguments]')}
+        Команды: ${chalk.blue(`
+          --version:                   # выводит номер версии
+          --help:                      # печатает этот текст
+          --import <path>:             # импортирует данные из TSV`)}`
+    );
   }
 }
