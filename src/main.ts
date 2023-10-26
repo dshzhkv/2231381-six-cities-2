8890c9a37abcd634326b0c0f6c9f236d1ef45a6c
dshzhkv <dashunich.f@gmail.com>
2023-10-26 08:55:09 +0500

    Сделала основной класс приложения, схему конфигурации

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import PinoService from './core/logger/pino.service.js';
+import RestApplication from './app/rest.js';
+import ConfigService from './core/config/config.service';
+
+async function bootstrap() {
+  const logger = new PinoService();
+  const config = new ConfigService(logger);
+
+  const application = new RestApplication(logger, config);
+  await application.init();
+}
+
+bootstrap();
